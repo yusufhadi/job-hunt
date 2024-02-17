33c09952d4d2906430c6d9c33a71a78d1485dcd6
yusufhadi <hadiyusuf889@gmail.com>
2024-02-17 12:32:14 +0800

    slicing FormSeacrhDynamic component

---
+++ src/components/organisms/FormSearchDynamic/index.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { LOCATION_OPTIONS } from "@/constants";
+import { optionType } from "@/types";
+import React, { FC } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+
+interface FormSearchDynamicProps {}
+
+const FormSearchDynamic: FC<FormSearchDynamicProps> = ({}) => {
+  return (
+    <div className="mx-auto w-max">
+      <div className="bg-background p-4 shadow-md inline-flex items-center gap-4 relative w-max z-10 text-center">
+        <div className="inline-flex gap-3 items-center">
+          <AiOutlineSearch className="w-6 h-6" />
+          <Input
+            className="py-5 w-[350px] border-none"
+            placeholder="Job Title or keyword"
+          />
+        </div>
+        <div className="inline-flex gap-3 items-center">
+          <HiOutlineLocationMarker className="w-6 h-6" />
+          <Select>
+            <SelectTrigger className="w-[350px] border-none outline-none py-5 text-gray-500">
+              <SelectValue placeholder="Select a location" />
+            </SelectTrigger>
+            <SelectContent>
+              {LOCATION_OPTIONS.map((item: optionType, i: number) => (
+                <SelectItem key={i} value={item.id}>
+                  {item.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div>
+          <Button>Search</Button>
+        </div>
+      </div>
+      <div>
+        <div className="text-muted-foreground mt-3">
+          Popular : UI Designer, UX Researcher, Android, Admin
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormSearchDynamic;
--- src/containers/ExploreDataContainers/index.tsx
+++ src/containers/ExploreDataContainers/index.tsx
@@ -1,3 +1,4 @@
+import FormSearchDynamic from "@/components/organisms/FormSearchDynamic";
 import Image from "next/image";
 import React, { FC } from "react";
 
@@ -33,7 +34,7 @@ const ExploreDataContainers: FC<ExploreDataContainersProps> = ({}) => {
           </div>
         </div>
         <div>
-          <div>Form Search</div>
+          <FormSearchDynamic />
         </div>
       </div>
       <div className="mt-20 mb-16 px-32 flex flex-row items-start gap-10">
