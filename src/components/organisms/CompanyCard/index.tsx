b12c88679dba90f7d68e6dd2db7dec91ad024438
yusufhadi <hadiyusuf889@gmail.com>
2024-02-19 14:45:05 +0800

    slicing detail-company page

--- src/components/organisms/CompanyCard/index.tsx
+++ src/components/organisms/CompanyCard/index.tsx
@@ -1,6 +1,7 @@
 import { Badge } from "@/components/ui/badge";
 import { companyType } from "@/types";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React, { FC } from "react";
 
 interface CompanyCardProps extends companyType {}
@@ -12,8 +13,13 @@ const CompanyCard: FC<CompanyCardProps> = ({
   name,
   totalJobs,
 }) => {
+  const router = useRouter();
+
   return (
-    <div className="border border-border p-6">
+    <div
+      onClick={() => router.push("/detail/company/1")}
+      className="border border-border p-6 cursor-pointer"
+    >
       <div className="flex flex-row items-start justify-between">
         <Image src={image} alt={image} height={66} width={66} />
         <Badge>{totalJobs} Jobs</Badge>
