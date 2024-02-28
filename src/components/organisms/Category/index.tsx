b69f83286f83944d877aedb7fad7873c19ff501b
yusufhadi <hadiyusuf889@gmail.com>
2024-02-28 15:12:00 +0800

    integration component category

--- src/components/organisms/Category/index.tsx
+++ src/components/organisms/Category/index.tsx
@@ -1,18 +1,32 @@
+"use client";
+
 import TitleSection from "@/components/atoms/TitleSection";
-import React, { FC } from "react";
-import { BiCategory } from "react-icons/bi";
-import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { FC, useMemo } from "react";
 import CategoryItem from "./CategoryItem";
+import { fetcher, parsingCategories } from "@/lib/utils";
+import useSWR from "swr";
+import { companyJobType } from "@/types";
 
 interface CategoryProps {}
 
 const Category: FC<CategoryProps> = ({}) => {
+  const { data, isLoading, error } = useSWR("/api/job/categories", fetcher);
+
+  const categories = useMemo(
+    () => parsingCategories(data, isLoading, error),
+    [data, isLoading, error]
+  );
+
   return (
     <div className="mt-32 mb-8">
       <TitleSection word1="Explore by" word2="category" />
       <div className="grid grid-cols-5 gap-9 mt-12">
-        {[0, 1, 2, 3, 4].map((item: number) => (
-          <CategoryItem key={item} name="Category" totalJobs={100} />
+        {categories.map((item: companyJobType) => (
+          <CategoryItem
+            key={item.id}
+            name={item.name}
+            totalJobs={item.totalJobs}
+          />
         ))}
       </div>
     </div>
